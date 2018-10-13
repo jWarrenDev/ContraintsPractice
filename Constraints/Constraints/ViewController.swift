49f82a9156a46798ca4f7aa30fa2ed29994b8503
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-10-13 16:05:46 -0700

    purple rectangle is not centered

--- Constraints/Constraints/ViewController.swift
+++ Constraints/Constraints/ViewController.swift
@@ -15,13 +15,15 @@ class ViewController: UIViewController {
         
         // Views have a frame property
         let greenSquare = UIView()
+        let purpleSquare = UIView()
         
         // always set this to false
-        //
         greenSquare.translatesAutoresizingMaskIntoConstraints = false
         greenSquare.backgroundColor = .green
         view.addSubview(greenSquare)
  
+       
+        
         // lets write all 4 constraints
         // Width, Height, CenterY, CenterX
         // Then we are going to activate them and pass them in
@@ -60,7 +62,7 @@ class ViewController: UIViewController {
                                                    multiplier: 1.0,
                                                    constant: 0.0)
         
-        // needs to be in an array
+        // needs to be passed in as param
         NSLayoutConstraint.activate([heightConstraint, widthConstraint, centerYConstraint, centerXConstraint])
         
         
@@ -68,7 +70,44 @@ class ViewController: UIViewController {
         // same width, centered horizontally
         // space below green square margin 10 points
         
+        purpleSquare.translatesAutoresizingMaskIntoConstraints = false
+        purpleSquare.backgroundColor = .purple
+        view.addSubview(purpleSquare)
         
+        let topConstraintP = NSLayoutConstraint(item: purpleSquare,
+                                                 attribute: .top,
+                                                 relatedBy: .equal,
+                                                 toItem: greenSquare,
+                                                 attribute: .bottom,
+                                                 multiplier: 1.0,
+                                                 constant: 10.0)
+        
+        let bottomConstraintP = NSLayoutConstraint(item: view,
+                                                  attribute: .bottom,
+                                                  relatedBy: .equal,
+                                                  toItem: purpleSquare,
+                                                  attribute: .bottom,
+                                                  multiplier: 1.0,
+                                                  constant: 10.0)
+        
+        let widthConstraintP = NSLayoutConstraint(item: purpleSquare,
+                                                   attribute: .width,
+                                                   relatedBy: .equal,
+                                                   toItem: greenSquare,
+                                                   attribute: .width,
+                                                   multiplier: 1.0,
+                                                   constant: 0.0)
+        
+        let heightConstraintP = NSLayoutConstraint(item: purpleSquare,
+                                                   attribute: .height,
+                                                   relatedBy: .equal,
+                                                   toItem: greenSquare,
+                                                   attribute: .height,
+                                                   multiplier: 1.0,
+                                                   constant: 0.0)
+        
+        // needs to be passed in as param
+        NSLayoutConstraint.activate([topConstraintP, bottomConstraintP, widthConstraintP, heightConstraintP])
     }
 
 }
